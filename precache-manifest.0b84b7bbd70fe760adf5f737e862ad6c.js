b1f7027d7e47e12dcd77252eb35d32e0dd6ce45f
Vogt, Nicolas (nv777x) <nv777x@att.com>
2019-03-28 14:44:48 -0700

    Updates

---
+++ precache-manifest.0b84b7bbd70fe760adf5f737e862ad6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/attvideo2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2e80be1d6e4b414c9dbe09a117987816",
+    "url": "/attvideo2/static/media/attvideoIcon.2e80be1d.png"
+  },
+  {
+    "revision": "0717e5464fa74b96c4e5",
+    "url": "/attvideo2/static/js/runtime~main.6efd2b23.js"
+  },
+  {
+    "revision": "2403f9f61d152b5cfd4d",
+    "url": "/attvideo2/static/js/main.3188a1d4.chunk.js"
+  },
+  {
+    "revision": "d9892706f52cf4e887bc",
+    "url": "/attvideo2/static/js/2.4d6b2547.chunk.js"
+  },
+  {
+    "revision": "2403f9f61d152b5cfd4d",
+    "url": "/attvideo2/static/css/main.cb923c88.chunk.css"
+  },
+  {
+    "revision": "4a4d3e1a5767b74405f07d2694873023",
+    "url": "/attvideo2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.a84090ecbf03fb2626e7e66e7227d1e8.js"
+  "/attvideo2/precache-manifest.0b84b7bbd70fe760adf5f737e862ad6c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/attvideo2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
